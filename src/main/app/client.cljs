fa26e5d218a5e2994a11239ce3f1d8d111cb40c5
Lucas Teles <lucas.teles@outlook.com>
2021-06-25 20:02:24 -0300

    WIP

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -6,8 +6,25 @@
 
 (defonce app (app/fulcro-app))
 
-(defsc Root [this props]
-  (dom/div "TODO"))
+(defsc Car [this {:car/keys [id model]}]
+  {}
+  (dom/li "Modelo: " model))
+(def ui-car (comp/factory Car {:key-fn :car/id}))
+
+(defsc Person [this {:person/keys [id name]}]
+  {:query [:person/id :person/name]
+   :ident :person/id}
+  (dom/div
+    (dom/div (str id " - " name))
+    #_#_ (dom/h4 "Carros:")
+    (dom/ul (map ui-car cars))))
+
+(def ui-person (comp/factory Person {:key-fn :person/id}))
+
+(defsc Root [this {:keys [sample]}]
+  {}
+  (dom/div (ui-person sample)))
+
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
@@ -23,3 +40,21 @@
   ;; As of Fulcro 3.3.0, this addition will help with stale queries when using dynamic routing:
   (comp/refresh-dynamic-queries! app)
   (js/console.log "Hot reload"))
+
+
+(comment
+  (shadow/repl :main)
+  (js/console.log "ola")
+  (keys shadow)
+  (js/alert "hi")
+
+  (-> (::app/state-atom app) deref)
+
+  (reset! (::app/state-atom app)
+          {:sample #:person {:id 1
+                             :name "Lucas"
+                             :cars [#:car{:id 22 :model "Opala"}
+                                    #:car{:id 23 :model "Caravan"}]}})
+
+  (app/schedule-render! app)
+  )
